a901214ead3c5c53d36aa5937df5925fa73eaa83
diggitydingdong <mitchellinminecraft@gmail.com>
2021-11-16 12:55:32 +1100

    Schemas and connectivity to a discord bot.

---
+++ db/index.js
@@ -0,0 +1,48 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+// Initialise all schemas. Needs to run in serial.
+const initialiseSchemas = () => {
+    db.serialize(() => {
+        // Guild - discord server.
+        db.run(`CREATE TABLE Guild (
+            guild_id TEXT NOT NULL PRIMARY KEY,
+            wr_message_id TEXT,
+            wr_role_color INT
+        )`);
+
+        // User - associating a speedrun.com account to a discord account
+        // (if discord_id is null then the account is unassociated)
+        db.run(`CREATE TABLE User (
+            user_id TEXT NOT NULL PRIMARY KEY,
+            src_username TEXT NOT NULL,
+            discord_id TEXT
+        )`);
+        
+        // Leaderboard - each leaderboard (game/category/variable) and its data. To be tracked by guilds.
+        db.run(`CREATE TABLE Leaderboard (
+            lb_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            game_id TEXT NOT NULL,
+            category_id TEXT NOT NULL,
+            wr_holder_id TEXT REFERENCES User (user_id),
+            wr_run_id TEXT
+        )`);
+
+        // LeaderboardVariable - Defining the variables for each leaderboard (subcategories)
+        db.run(`CREATE TABLE LeaderboardVariable (
+            lb_id INTEGER NOT NULL REFERENCES Leaderboard (lb_id),
+            variable_id TEXT NOT NULL,
+            value TEXT NOT NULL,
+            PRIMARY KEY (lb_id, variable_id)
+        )`);
+
+        // TrackedLeaderboard - which guilds track which leaderboards?
+        db.run(`CREATE TABLE TrackedLeaderboard (
+            guild_id TEXT NOT NULL REFERENCES Guild (guild_id),
+            lb_id INT NOT NULL REFERENCES Leaderboard (lb_id),
+            PRIMARY KEY (guild_id, lb_id)
+        )`)
+    })
+}
+
+module.exports = { initialiseSchemas }
---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client } = require('discord.js');
+const client = new Client({ intents: [] });
+
+const { initialiseSchemas } = require('./db');
+const { token } = require('./auth.json');
+
+// Initialisation code to be run after the discord client has logged in.
+const init = () => {
+    // Log all the guilds the bot is in.
+    console.log(`Client is ready in the following Guilds: [${client.guilds.cache.size}]`);
+    client.guilds.cache.forEach(g => {
+        console.log(`[${g.id}] ${g.available ? g.name : "UNAVAILABLE"} `)
+    });
+
+    // Database
+    console.log("Setting up the SQLite database...");
+    initialiseSchemas();
+
+    // Interact with the src API and update world records accordingly
+};
+
+client.once('ready', init);
+client.login(token);
