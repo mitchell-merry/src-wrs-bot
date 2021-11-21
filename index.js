4e59f1453798d5f1a1fad7985088c8904c9aee4a
diggitydingdong <mitchellinminecraft@gmail.com>
2021-11-21 19:55:22 +1100

    Cleanup index.js

--- index.js
+++ index.js
@@ -22,79 +22,7 @@ const init = async () => {
     console.log("Database successfully initialised.");
 
     updateGuild('867962530964848680');
-
-    // console.log(await getAllLeaderboardsForGuild(sequelize.models, '705780146216370326'));
-
-    // const { Guild, Leaderboard, Variable } = sequelize.models;
-
-    // Guild.findByPk('705780146216370326', {
-    //     include: {
-    //         model: Leaderboard,
-    //         through: { attributes: [] },
-    //         attributes: { exclude: [ 'lb_id', 'wr_holder_id', 'wr_run_id' ] },
-    //         include: [{ 
-    //             model: Variable,
-    //             attributes: { exclude: ['lb_id'] }
-    //         }]
-    //     },
-    //     attributes: {
-    //         exclude: [ 'guild_id', 'wr_message_id', 'wr_role_color' ]
-    //     }
-    // })
-    // .then((res) => console.log(JSON.stringify(res, null, 2)));
-
-    // await sequelize.models.Leaderboard.findAll()
-    //     .then((res) => console.log(JSON.stringify(res, null, 2)));
-
-    // Interact with the src API and update world records accordingly
 };
 
 client.once('ready', init);
 client.login(token);
-
-// Temp stuff playing around with
-
-/*
-const G = await Guild.findByPk(guild_id, {
-        include: {
-            model: Leaderboard,
-            through: { attributes: [] },
-            include: [{ 
-                model: Variable,
-                exclude: ["lb_id"]
-            }, {
-                model: User,
-                as: "WRHolder"
-            }]
-        }
-    })
-    */
-
-const getAllLeaderboardsForGuild = async (models, guild_id) => {
-    const { Guild, Leaderboard, Variable, User } = models;
-    
-    const G = await Guild.findByPk(guild_id, {
-        include: {
-            model: Leaderboard,
-            through: { attributes: [ ] },
-            attributes: { exclude: [ 'lb_id' ] },
-            include: [{ 
-                model: Variable,
-                attributes: { exclude: ['lb_id'] }
-            }]
-        },
-        attributes: {
-            exclude: [ 'guild_id', 'wr_message_id', 'wr_role_color' ]
-        }
-    });
-    console.log(JSON.stringify(G, null, 2));
-
-    const lb_names = [];
-    for(const board of G.Leaderboards) {
-        const b = { game_id: board.game_id, category_id: board.category_id, variables: [ ...board.Variables ] }
-        // console.log(JSON.stringify(b, null, 2));
-        lb_names.push(await getLeaderboardName(b));
-    }
-
-    return lb_names;
-}
